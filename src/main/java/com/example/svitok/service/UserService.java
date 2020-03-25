fa7739fb7dc644ecff29372b7e9db720f72a6d3d
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-03-25 12:46:12 +0300

    + ManyToMany subscriptions
    Добавлены подписки и подписчики

--- src/main/java/com/example/svitok/service/UserService.java
+++ src/main/java/com/example/svitok/service/UserService.java
@@ -79,4 +79,14 @@ public class UserService implements UserDetailsService {
         }
         userRepository.save(user);
     }
+
+    public void subscribe(User currentUser, User user) {
+        user.getSubscribers().add(currentUser);
+        userRepository.save(user);
+    }
+
+    public void unsubscribe(User currentUser, User user) {
+        user.getSubscribers().remove(currentUser);
+        userRepository.save(user);
+    }
 }
